5f032765f59e04e6ef0147df5633f8ab6f625e05
aekobear <aekobear@gmail.com>
2025-02-14 17:07:46 -0500

    Add debug

--- lib/qbittorrent/web_api/torrent.rb
+++ lib/qbittorrent/web_api/torrent.rb
@@ -10,7 +10,9 @@ module QBittorrent
       def add_torrents(url_or_file_path, **options)
         send_request('/torrents/add') do |api_url|
           url_or_file_path.gsub!(/^\s+|\s+$/, '')
+          puts "HERE"
           if %r{^http://|https://|magnet:}i =~ url_or_file_path
+            puts "GOT HERE TOO"
             options.merge!(urls: url_or_file_path)
           else
             options.merge!(torrents: HTTP::FormData::File.new(url_or_file_path))
